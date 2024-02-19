0a17c186dfae2cb2e507dfbe37c81f927aadc78b
manonbalard <122499111+manonbalard@users.noreply.github.com>
2024-02-19 15:21:36 +0000

    added css and html pages

--- app.py
+++ app.py
@@ -6,10 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route("/about")
-def about():
-    return render_template('about.html')
+@app.route("/recettes")
+def recettes():
+    return render_template('recettes.html')
 
+@app.route("/login")
+def login():
+    return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
