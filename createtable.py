ffb6fd7ed5616476d5bbe145025617cadfad8753
Manon Balard <balard.manon@gmail.com>
2024-02-22 12:34:10 +0100

    redesigned the form to add a new recipe

--- createtable.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('recettes.db')
-print("Connected successfully")
-
-conn.execute('CREATE TABLE recettes (titre TEXT, ingrédients TEXT, étapes TEXT)')
-print("Created table successfully!")
-
-conn.close()
--- myflaskapp.py
+++ myflaskapp.py
@@ -12,13 +12,22 @@ database = SQLAlchemy(app)
 class Data(database.Model):
     id = database.Column(database.Integer, primary_key = True)
     titre = database.Column(database.String(100))
-    ingrédients = database.Column(database.String(100))
     étapes = database.Column(database.String(100))
 
-    def __init__(self,titre,ingrédients, étapes):
+    def __init__(self,titre, étapes):
         self.titre = titre
-        self.ingrédients = ingrédients
         self.étapes = étapes
+
+class Ingrédients(database.Model):
+    id = database.Column(database.Integer, primary_key = True)
+    quantité = database.Column(database.String(100))
+    unité = database.Column(database.String(100))
+    ingrédient = database.Column(database.String(100))
+
+    def __init__(self, quantité, unité, ingrédient):
+        self.quantité = quantité
+        self.unité = unité
+        self.ingrédient = ingrédient
 
 @app.route("/")
 def home():
@@ -32,14 +41,30 @@ def enternewrecipe():
 def addrecipe():
     if request.method == 'POST':
             titre = request.form['titre']
-            ingrédients = request.form['ingrédients']
             étapes = request.form['étapes']
 
-            my_data = Data(titre, ingrédients, étapes)
+            my_data = Data(titre, étapes)
             database.session.add(my_data)
             database.session.commit()
 
             return redirect(url_for('recipes'))
+
+@app.route("/addingredient", methods = ['POST'])
+def addingredient():
+    if request.method == 'POST':
+            quantité = request.form['quantité']
+            unité = request.form['unité']
+            ingrédient = request.form['ingrédient']
+
+            my_list_of_ingredients= Ingrédients(quantité, unité, ingrédient)
+            database.session.add(my_list_of_ingredients)
+            database.session.commit()
+
+            return redirect(url_for('enternewrecipe'))
+
+
+
+
 
 @app.route("/recipes")
 def recipes():
