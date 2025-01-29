5fd7387934e6721a65181bc4d13f5f118ae52f04
Manon Balard <balard.manon@gmail.com>
2025-01-29 17:10:05 +0100

    added Atkinson font

--- tests/test_security.py
+++ tests/test_security.py
@@ -1,7 +1,3 @@
-import io
-from flask_login import login_user
-
-
 def test_sql_injection(test_client):
     """Test to check protection against SQL injection."""
     payload = "' OR '1'='1"
@@ -42,43 +38,9 @@ def test_weak_password_rejection(test_client, user):
     )  # Vérifier le message d'erreur
 
 
-def test_file_upload_restriction_add_recipe(test_client, user):
-    """Test to verify that only allowed file types can be uploaded when adding a recipe."""
-
-    # Connecter l'utilisateur et vérifier la session avec test_client
-    with test_client:
-        # Connexion de l'utilisateur en simulant une requête avec le test client
-        login_user(user)
-        assert "_user_id" in test_client.session
-
-        # Créer des données de test avec un fichier malveillant
-        data = {
-            "title": "Test Recipe",
-            "description": "A test recipe",
-            "ingredients": "[]",  # Exemple d'ingrédient vide
-            "recipeImage": (io.BytesIO(b"fake content"), "malicious.exe"),
-        }
-
-        # Effectuer un POST vers la route 'addrecipe_with_ingredients'
-        response = test_client.post(
-            "/addrecipe_with_ingredients", data=data, content_type="multipart/form-data"
-        )
-
-        # Vérifier si la réponse contient le message d'erreur concernant le type de fichier
-        assert b"File type not allowed" in response.data
-
-
 def test_error_handling(test_client):
     """Test to ensure that errors do not reveal sensitive information."""
     response = test_client.get("/nonexistent-page")
     assert (
         b"Internal Server Error" not in response.data
     )  # Verify that technical details are not exposed
-
-
-def test_secure_session_cookie(test_client):
-    """Test to check that session cookies are secure."""
-    response = test_client.get("/")
-    session_cookie = response.headers.get("Set-Cookie")
-    assert "HttpOnly" in session_cookie
-    assert "Secure" in session_cookie
